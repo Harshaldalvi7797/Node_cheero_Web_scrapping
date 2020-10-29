05c10f8e128e9f4ac1568862080a1c12d1cafa95
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-10-29 22:45:01 +0530

    commit : response not

--- scrapp.js
+++ scrapp.js
@@ -1,2 +1,12 @@
 const request = require("request");
 const cheerio = require("cheerio");
+request(
+  "https://webcompletesolutions.com/index.html",
+  (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      const subtitle = $("sub-title");
+      console.log(subtitle);
+    }
+  }
+);
